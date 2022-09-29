ffc2805e07808c77f3f724f8aea8e02e4abe1e31
Jia-Yin <jywglady@gmail.com>
2022-09-29 10:44:40 +0800

    Add RGB light

---
+++ Arduino/RGBlight/RGBlight.ino
@@ -0,0 +1,29 @@
+// RGB light programmed by JYW
+
+#define RED 13
+#define YELLOW 12
+#define GREEN 11
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(RED, HIGH);
+  delay(1000);
+  digitalWrite(RED, LOW);
+  delay(1000);
+  digitalWrite(YELLOW, HIGH);
+  delay(1000);
+  digitalWrite(YELLOW, LOW);
+  delay(1000);
+  digitalWrite(GREEN, HIGH);
+  delay(1000);
+  digitalWrite(GREEN, LOW);
+  delay(1000);
+}
