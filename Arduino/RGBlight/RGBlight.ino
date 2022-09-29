5e446fa93066366a0bb4023e98784d6f4e919378
Jia-Yin <jywglady@gmail.com>
2022-09-29 12:14:52 +0800

    RGB Blink

--- Arduino/RGBlight/RGBlight.ino
+++ Arduino/RGBlight/RGBlight.ino
@@ -5,25 +5,29 @@
 #define GREEN 11
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(RED, OUTPUT);
-  pinMode(YELLOW, OUTPUT);
-  pinMode(GREEN, OUTPUT);
+void setup()
+{
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(RED, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+}
+
+void blink(int light, int duration, int times)
+{
+    for (int i = 0; i < times; i++)
+    {
+        digitalWrite(light, HIGH);
+        delay(duration);
+        digitalWrite(light, LOW);
+        delay(duration);
+    }
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(RED, HIGH);
-  delay(1000);
-  digitalWrite(RED, LOW);
-  delay(1000);
-  digitalWrite(YELLOW, HIGH);
-  delay(1000);
-  digitalWrite(YELLOW, LOW);
-  delay(1000);
-  digitalWrite(GREEN, HIGH);
-  delay(1000);
-  digitalWrite(GREEN, LOW);
-  delay(1000);
+void loop()
+{
+    blink(RED, 250, 3);
+    blink(YELLOW, 250, 3);
+    blink(GREEN, 250, 3);
 }
